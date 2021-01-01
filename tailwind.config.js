a2915eeb6f4cac59a9ae5919d1feb86e37a4bc82
kamelia ait yahia <kamelia.ait.yahia.1998@gmail.com>
2021-01-01 02:16:45 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors:{
+        softred:'#E19999',
+        sparkle:{
+          100:'#C7D0C2',
+          200:'#4A5759',
+        }
+      },
+      fontFamily:{
+        body:['Cabin'],
+      } 
+    },
+  },
+  variants: {},
+  plugins: [],
+}
